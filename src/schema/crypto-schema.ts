5c6dc0b78bf17e5b7679b09f674a509b47d22f35
Omar <omar.garcia@morsa.com.mx>
2025-12-19 13:43:25 -0600

    Validando la RESPUESTA de la API con ZOD

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -2,5 +2,32 @@ import { z } from "zod";
 
 export const CurrencySchema = z.object({
   code: z.string(),
-  name: z.string()
+  name: z.string(),
 });
+
+export const CryptoCurrencyResponseSchema = z.object({
+  CoinInfo: z.object({
+    FullName: z.string(),
+    Name: z.string(),
+  }),
+});
+
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema);
+
+// Nueva Url
+// export const CurrencySchema = z.object({
+//   ID: z.number(),
+//   NAME: z.string(),
+//   SYMBOL: z.string(),
+//   PRICE_USD: z.number(),
+//   CHANGE_24H_PERCENT: z.number().optional(),
+// });
+
+
+// export const CryptoCurrencyResponseSchema = z.object({
+//   Data: z.object({
+//     LIST: z.array(CurrencySchema),
+//   }),
+// });
+
+
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,24 @@
 import axios from "axios";
 import { create } from "zustand";
+import { CryptoCurrenciesResponseSchema } from "./schema/crypto-schema";
 
 async function getCryptos() {
-  // const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
-  const url = "https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD";
+  const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
   const {data: {Data}} = await axios(url);
   console.log(Data);
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data);
+  console.log(result);
+
+  // Nueva Url
+  // const url = "https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD";
+  // const { data } = await axios(url);
+  // const result = CryptoCurrencyResponseSchema.safeParse(data);
+  // console.log(result.data.Data.LIST);
 };
 
 export const useCryptoStore = create(() => ({
   fetchCryptos: () => {
-    console.log('Desde FetchCryptos');
+    console.log("Desde FetchCryptos");
     getCryptos();
-  }
-}));  
+  },
+}));

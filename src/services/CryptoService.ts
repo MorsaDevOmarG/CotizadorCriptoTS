7e1a5f72efe1e8a27d8a097da5abad234ab05bd1
Omar <omar.garcia@morsa.com.mx>
2025-12-19 17:19:25 -0600

    Validando la RESPUESTA de la API con ZOD

--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CryptoCurrenciesResponseSchema } from "../schema/crypto-schema";
+import { CryptoCurrenciesResponseSchema, CryptoPriceShema } from "../schema/crypto-schema";
 import { Pair } from "../types";
 
 export async function getCryptos() {
@@ -28,5 +28,12 @@ export async function fetchCurrentCryptoPrice(pair: Pair) {
   const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.criptocurrency}&tsyms=${pair.currency}`;
 
   const { data: { DISPLAY } } = await axios(url);
-  console.log(DISPLAY[pair.criptocurrency][pair.currency]);
-}
+  // console.log(DISPLAY[pair.criptocurrency][pair.currency]);
+
+  const result = CryptoPriceShema.safeParse(DISPLAY[pair.criptocurrency][pair.currency]);
+  // console.log(result);
+
+  if (result.success) {
+    return result.data;
+  }
+};

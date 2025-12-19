4191aa8d4d3c57fafd2833986309e0db279873e5
Omar <omar.garcia@morsa.com.mx>
2025-12-19 17:25:12 -0600

    Colocando la RESPUESTA en el STATE

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,22 @@
 
 import { create } from "zustand";
 // import { CryptoCurrencies, CryptoCurrency } from "./types";
-import { CryptoCurrency, Pair } from "./types";
+import { CryptoCurrency, CryptoPrice, Pair } from "./types";
 import { devtools } from "zustand/middleware";
 import { fetchCurrentCryptoPrice, getCryptos } from "./services/CryptoService";
 
 type CryptoStore = {
   cryptocurrencies: CryptoCurrency[];
   // cryptocurrencies: CryptoCurrencies}
+  result: CryptoPrice;
   fetchCryptos: () => Promise<void>;
-  fetchData: (pair: Pair) => Promise<void>
+  fetchData: (pair: Pair) => Promise<void>;
+
 };
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
   cryptocurrencies: [],
+  result: {} as CryptoStore,
   fetchCryptos: async () => {
     // console.log("Desde FetchCryptos");
 
@@ -27,6 +30,11 @@ export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
   fetchData: async (pair) => {
     // console.log(pair);
 
-    await fetchCurrentCryptoPrice(pair);
+    const result = await fetchCurrentCryptoPrice(pair);
+    // console.log(result);
+
+    set(() => ({
+      result
+    }));
   }
 })));
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { CryptoCurrenciesResponseSchema, CryptoCurrencyResponseSchema, CurrencySchema, PairSchema } from '../schema/crypto-schema';
+import { CryptoCurrenciesResponseSchema, CryptoCurrencyResponseSchema, CryptoPriceShema, CurrencySchema, PairSchema } from '../schema/crypto-schema';
 
 export type Currency = z.infer<typeof CurrencySchema>;
 
@@ -8,3 +8,5 @@ export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>;
 export type CryptoCurrencies = z.infer<typeof CryptoCurrenciesResponseSchema>;
 
 export type Pair = z.infer<typeof PairSchema>;
+
+export type CryptoPrice = z.infer<typeof CryptoPriceShema>;
